a0cd6ce7e1f3bc63c3e1bc553cfcae1c1e3eb812
adityayadav71 <adityayadav1088@gmail.com>
2022-09-26 10:06:05 +0530

    Updated database config

--- db_connection.php
+++ db_connection.php
@@ -1,8 +1,8 @@
 <?php
-$user = 'root';
-$password = ''; 
-$database = 'voterportal'; 
-$servername='localhost:3306';
+$user = 'sql3522256';
+$password = '9P5537h7Bx'; 
+$database = 'sql3522256'; 
+$servername='sql3.freesqldatabase.com';
 $mysqli = new mysqli($servername, $user, $password, $database);
 if ($mysqli->connect_error) {die('Connect Error (' . $mysqli->connect_errno . ') '. $mysqli->connect_error);} 
 ?>
